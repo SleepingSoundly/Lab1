f5288bb82658e2d608ece9d075f33a82ff818e72
Lia <ajghoag@gmail.com>
2017-09-16 16:29:33 -0700

    segfaults after adding virDomainGetInfo

--- CPU/vcpu_scheduler.c
+++ CPU/vcpu_scheduler.c
@@ -9,12 +9,31 @@
 #include <libvirt/libvirt.h>
 #include <stdlib.h>
 
+void printNodeInfo(virNodeInfoPtr info){
+	printf(" n : model == %s\n", info->model);
+	printf(" n : memory size  == %lu kb\n", info->memory);
+	printf(" n : # of active CPUs == %d\n", info->cpus);
+	printf(" n : expected CPU frequency  == %d mHz\n", info->mhz);
+
+}
+
+void printDomainInfo(virDomainInfoPtr info){
+	printf(" d : running state == %c\n", info->state);
+	printf(" d : maximum Memory == %lu Kb\n", info->maxMem);
+	printf(" d : memory in use == %lu Kb\n", info->memory);
+	printf(" d : # virtual CPUs == %lu Kb\n", info->nrVirtCpu);
+	printf(" d : CPU time used == %llu nanoseconds\n", info->cpuTime);
+	
+}
+
 int main(int argc, char *argv[]){
 	
 	int numDomains;
 	int ret;
 	int flags;
-	int itr;
+	int itr, itl;
+	int nVCPUs = 0;
+	int nparams = 0;
 	
 	virConnectPtr conn; // data structure for opening a connection for full r/w access (app creds needed)
 	
@@ -22,11 +41,12 @@ int main(int argc, char *argv[]){
 	virDomainPtr dom = NULL; // pointer to a virtual domain, obtained by name or ID
 	
 	virDomainPtr* domains = NULL; // for list of all domains returned by ListAll API	
-	int nVCPUs = 0;
-	int nparams = 0;
+	virNodeInfoPtr info;
+	virDomainInfoPtr Dinfo;
 	virTypedParameterPtr params;	
 
 	conn = virConnectOpen("qemu:///system"); // connect to the hypervisor
+
 
 	if ( conn == NULL ){
 		fprintf(stderr, "Failed to open connection to qemu\n");
@@ -35,6 +55,18 @@ int main(int argc, char *argv[]){
 	else{
 		printf("Success!\n");
 	}
+
+	ret = virNodeGetInfo(conn, info);
+	if ( ret != 0 ){
+		fprintf(stderr, "Failed to get Node info\n");
+		return 1;
+	}
+	else{
+		printf("Success!\n");
+	}
+
+
+	printNodeInfo(info);
 
 	numDomains = virConnectNumOfDomains(conn);
 	printf(" + %d domains connected\n", numDomains);
@@ -49,29 +81,39 @@ int main(int argc, char *argv[]){
 	}	
 	for(itr = 0; itr < numDomains; itr++){
 		dom = domains[itr];
+		ret = virDomainGetInfo(dom, Dinfo);
+		if ( ret == -1){
+			fprintf(stderr, "Failed to get Domain info\n");
+			return 1;
+		}
+
 		printf(" ++ %d: Starting analysis\n", itr);
 		// total statistics, arguments to get-stats allow us to get 
 		// number of cpus to query on the VM
 		nparams = virDomainGetCPUStats(dom, NULL, 0, -1, 1, 0); 
-		printf(" ++ %d: %d parameters to query on this domain\n", itr, ret);
+		printf(" ++ %d: %d parameters to query on this domain\n", itr, nparams);
 			
 
 		//  arguments let us return the number of virtual CPUs on 
 		// the vm
 		nVCPUs = virDomainGetCPUStats(dom, NULL, 0, 0, 1, 0); 
-		printf(" ++ %d: %d CPUs to query on this domain\n", itr, ret);
-	
-		params = calloc(nparams, sizeof(virTypedParameter));
-		ret = virDomainGetCPUStats(dom, params, nparams, -1, 1, 0); 
-		printf(" +++ %d: %s: %lu \n", itr, params->field, params->value);			
+		printf(" ++ %d: %d CPUs to query on this domain\n", itr, nVCPUs);
+			
+		params = calloc(nparams*nVCPUs, sizeof(virTypedParameter));
+		ret = virDomainGetCPUStats(dom, params, nparams, 0, nVCPUs, 0); 
+
+		for(itl = 0; itl < nparams*nVCPUs; itl++, params++){ 
+			printf(" +++ %d : %s-> %lu \n", itr, params->field, params->value);
+		}
 
 		printf(" ++ %d: freeing domain\n\n", itr);
-	
+		free(params);	
 		virDomainFree(dom);
 	}
 
  	// after obtaining domains, they must be freed because the function
 	// does an allocation per domain
+	free(info);
 	free(domains);	
 	virConnectClose(conn);
 
